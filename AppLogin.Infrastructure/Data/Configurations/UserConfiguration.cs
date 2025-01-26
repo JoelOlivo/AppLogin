293967a8ba1ecbd0adfdc886ca18b2b0bdbb402a
JoelOlivo <139314609+JoelOlivo@users.noreply.github.com>
2025-01-26 15:57:31 -0500

    Infrastructure v1.0

---
+++ AppLogin.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using AppLogin.Domain.Entities;
+
+namespace AppLogin.Infrastructure.Data.Configurations
+{
+    public class UserConfiguration
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Id)
+                .IsRequired();
+
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasIndex(u => u.Email)
+                .IsUnique();
+
+            builder.Property(u => u.Password)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(u => u.LastName)
+                .IsRequired(false)
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ AppLogin.Infrastructure/Data/DbContexts/AppLoginDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AppLogin.Domain.Entities;
+using AppLogin.Infrastructure.Data.Configurations;
+
+namespace AppLogin.Infrastructure.Data.DbContexts
+{
+    public class AppLoginDbContext : DbContext
+    {
+        public AppLoginDbContext(DbContextOptions<AppLoginDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+        }
+    }
+}
